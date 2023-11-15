eda05ee71b35a6f51ab6c116131ce0d87594b2ed
Gillebo01 <gillebo01@gmail.com>
2023-11-15 21:25:59 +0100

    deleted old obsolete code

--- Oppgaver/ZUMO test/ZUMO test.ino
+++ Oppgaver/ZUMO test/ZUMO test.ino
@@ -1,113 +1,185 @@
-/*If a motor on your Zumo has been flipped, you can correct its
-direction by uncommenting the call to flipLeftMotor() or
-flipRightMotor() in the setup() function. */
-
-#include <Wire.h>
+// Line Sensor Example
 #include <Zumo32U4.h>
+#include <Wire.h>
 
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4Encoders encoders;
+Zumo32U4ButtonB buttonB;
+Zumo32U4OLED display;
+
+unsigned long lastMillis = 0;
+unsigned long lastMillisBat = 0;
+float motorSpeed = 0;
+long lastPos = 0;
+float lastPosBat = 0;
+long travel = 0;
+float batteryLevel = 100;
+float batteryDrain = 0;
+const char encoderErrorLeft[] PROGMEM = "!<c2";
+const char encoderErrorRight[] PROGMEM = "!<e2";
+long totCountsLeft = 0;
+long totCountsRight = 0;
+// Sensor values will go in this array
+unsigned int lineSensorValues[5];
+// int sensVal = 0;
+int speed1 = 75;
+// int speed2 = 150;
+// int speed3 = 200;
+// int mid = 2000;
 
 void setup()
 {
-  // Uncomment if necessary to correct motor directions:
-  // motors.flipLeftMotor(true);
-  // motors.flipRightMotor(true);
-
-  // Wait for the user to press button A.
-  buttonA.waitForButton();
-  Serial.begin(9600);
-
-  // Delay so that the robot does not move away while the user is
-  // still touching it.
-  delay(1000);
+        Serial.begin(9600);
+        buttonA.waitForButton();
+        // Initialize the sensors, and turn on the LEDs for them.
+        lineSensors.initFiveSensors();
+        lineSensors.emittersOn();
+        // Calibrates the linesensors
+        calibrate();
+        delay(1000);
 }
 
-void runForward()
+void calibrate()
 {
-  ledYellow(1);
-  for (int speed = 0; speed <= 200; speed++)
-  {
-    motors.setLeftSpeed(speed);
-    motors.setRightSpeed(speed);
-    delay(2);
-  }
-  for (int speed = 200; speed >= 0; speed--)
-  {
-    motors.setLeftSpeed(speed);
-    motors.setRightSpeed(speed);
-    delay(2);
-  }
+        for (int i = 100; i < 200; i++)
+        {
+                motors.setSpeeds(i, -i);
+                lineSensors.calibrate();
+        }
+
+        for (int i = 200; i > 0; i--)
+        {
+                motors.setSpeeds(i, -i);
+                lineSensors.calibrate();
+        }
+
+        for (int i = 100; i < 200; i++)
+        {
+                motors.setSpeeds(-i, i);
+                lineSensors.calibrate();
+        }
+
+        for (int i = 200; i > 0; i--)
+        {
+                motors.setSpeeds(-i, i);
+                lineSensors.calibrate();
+        }
 }
 
-void turnLeft()
+void OLED()
 {
-  ledYellow(0);
-  for (int speed = 0; speed >= -200; speed--)
-  {
-    motors.setRightSpeed(speed);
-    motors.setLeftSpeed(-speed);
-    delay(2);
-  }
-  for (int speed = -200; speed <= 0; speed++)
-  {
-    motors.setRightSpeed(speed);
-    motors.setLeftSpeed(-speed);
-    delay(2);
-  }
+        int16_t position = lineSensors.readLine(lineSensorValues);
+        static uint8_t lastDisplayTime;
+
+        if ((uint8_t)(millis() - lastDisplayTime) >= 100)
+        {
+                lastDisplayTime = millis();
+                display.clear();
+                display.setLayout21x8();
+                display.gotoXY(0, 0);
+                display.print(travel);
+                display.setLayout21x8();
+                display.gotoXY(4, 0);
+                display.print("cm");
+
+/*              display.setLayout21x8();
+                display.gotoXY(0, 1);
+                display.print(motorSpeed);
+                display.gotoXY(4, 1);
+                display.print("cm/s");
+*/
+                display.setLayout21x8();
+                display.gotoXY(0, 1);
+                display.print(round(batteryLevel),1);
+                display.setLayout21x8();
+                display.gotoXY(4, 1);
+                display.print("%");
+        }
 }
 
-void turnRight()
+void calculations()
 {
-  ledYellow(0);
-  for (int speed = 0; speed >= -200; speed--)
-  {
-    motors.setRightSpeed(-speed);
-    motors.setLeftSpeed(speed);
-    delay(2);
-  }
-  for (int speed = -200; speed <= 0; speed++)
-  {
-    motors.setRightSpeed(-speed);
-    motors.setLeftSpeed(speed);
-    delay(2);
-  }
-}
+        totCountsLeft += encoders.getCountsAndResetLeft();
+        totCountsRight += encoders.getCountsAndResetRight();
 
-void turnAround()
-{
-  ledYellow(0);
-  for (int speed = 0; speed >= -350; speed--)
-  {
-    motors.setRightSpeed(speed);
-    motors.setLeftSpeed(-speed);
-    delay(2);
-  }
-  for (int speed = -350; speed <= 0; speed++)
-  {
-    motors.setRightSpeed(speed);
-    motors.setLeftSpeed(-speed);
-    delay(2);
-  }
+        travel = ((totCountsLeft + totCountsRight) / 2) / 7.9 / 10;
+
+        unsigned long currentMillis = millis();
+        float currentPos = (totCountsLeft + totCountsRight) / 2;
+        if (currentMillis - lastMillis >= 100)
+        {
+                motorSpeed = (((currentPos - lastPos) / 7.9) / (currentMillis - lastMillis)) * 100;
+            if (batteryLevel > 0){   
+                batteryLevel -= 0.1 + (currentPos-lastPos) / 10000 * motorSpeed;} 
+
+                lastMillis = currentMillis;
+                lastPos = currentPos;
+        }
+        //Serial.println(motorSpeed);
+        Serial.println(batteryLevel);
 }
 
 void RUN()
 {
-  runForward();
-  delay(2);
-  turnLeft();
-  delay(2);
-  turnAround();
-  delay(2);
-  runForward();
-  delay(2);
-  turnRight();
-  delay(2);
-  runForward();
-  delay(500);
+        int16_t position = lineSensors.readLine(lineSensorValues);
+
+        // Mid
+        if (1700 < position < 2300)
+        {
+                motors.setSpeeds(speed1, speed1);
+        }
+
+        // Høyre
+
+        if (position < 1700)
+        {
+                motors.setSpeeds(speed1 * 0.5, speed1 * 1.5);
+        }
+
+        if (position <= 1000)
+        {
+                motors.setSpeeds(speed1 * 0.25, speed1 * 2);
+        }
+
+        // Venstre
+
+        if (position > 2300)
+        {
+                motors.setSpeeds(speed1 * 1.5, speed1 * 0.5);
+        }
+
+        if (position >= 3000)
+        {
+                motors.setSpeeds(speed1 * 2, speed1 * 0.25);
+        }
+
+        // Serial.println(position);
+}
+
+void battery()
+{
+       /* unsigned long currentMillisBat = millis();
+        float currentPosBat = (totCountsLeft + totCountsRight) / 2;
+        if (currentMillisBat - lastMillisBat >= 100)
+        {
+                batteryLevel -= 0.1;// + (abs(currentPosBat-lastPosBat) / 1000) * motorSpeed;
+                // int batteryLevel -= batteryDrain;
+                //  Serial.println(batteryDrain);
+                lastMillisBat = currentMillisBat;
+                lastPosBat = currentPosBat;
+
+                //Serial.println(round(batteryLevel), 2);
+               
+
+        }*/
 }
 
 void loop()
 {
-  RUN();
+        RUN();
+        OLED();
+       // battery();
+        calculations();
 }
